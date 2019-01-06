a6b234e10d7876c250b6fc1181852630ce9ddc54
marunium <ishimaru0510@gmail.com>
2019-01-06 13:27:10 +0900

    [アセット追加]　Post Processing Stack

---
+++ Rg01/Assets/UI/Scripts/AllyStatusViewController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AllyStatusViewController : MonoBehaviour {
+
+	[SerializeField]
+	Slider HpGauge;
+	[SerializeField]
+	Slider SPGauge;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		HpGauge.value = 1;
+		SPGauge.value = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (HpGauge.value > 0)
+		{
+			HpGauge.value -= 0.01f;
+		}
+		if (SPGauge.value > 0)
+		{
+			SPGauge.value -= 0.01f;
+		}
+	}
+}
